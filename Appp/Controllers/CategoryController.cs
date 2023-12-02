9d798cbdb485a4e7bdc15e21000bd45385e3ea92
AlbertZaqaryan <itm.albert.zaqaryan@gmail.com>
2023-12-02 18:16:10 +0400

    v1

---
+++ Appp/Controllers/CategoryController.cs
@@ -0,0 +1,34 @@
+﻿using Appp.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Appp.Controllers
+{
+	public class CategoryController : Controller
+	{
+		private readonly ApplicationDbContext _context;
+
+		public CategoryController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		public IActionResult Index()
+		{
+			var	categories = _context.categories.ToList();
+			return View(categories);
+		}
+
+		public IActionResult Details(int id) 
+		{
+			var category = _context.categories.Find(id);
+
+			if(category == null)
+			{
+				return NotFound();
+			}
+
+			return View(category);
+		
+		}
+	}
+}
---
+++ Appp/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using Appp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Appp.Controllers
+{
+	public class ProductController : Controller
+	{
+		private readonly ApplicationDbContext _context;
+
+		public ProductController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		public IActionResult Index()
+		{
+			var product = _context.products.Include(p => p.Category).ToList();
+
+			if (product == null) 
+			{
+				return NotFound();
+			
+			}
+			return View(product);
+		}
+
+		public IActionResult Details(int id) 
+		{
+			var product = _context.products.Include(p => p.Category).FirstOrDefault(p => p.ProductId == id);
+
+			if(product == null)
+			{
+				return NotFound();
+			}
+			return View(product);
+		}
+	}
+}
